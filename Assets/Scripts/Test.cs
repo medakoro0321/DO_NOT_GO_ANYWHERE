a483462ff073aa74cdfb386242aff40470b898a0
めだころ <153439300+medakoro0321@users.noreply.github.com>
2025-09-23 12:10:05 +0900

    init commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Test : UdonSharpBehaviour
+{
+    private bool isHeld = false;
+
+    ///<summary>
+    ///持った時呼ばれる
+    /// <summary>
+    public override void OnPickup()
+    {
+        isHeld = true;
+        Debug.Log("持ったよ");
+    }
+
+    /// <summary>
+    /// 落とした時
+    /// <summary>
+    public override void OnDrop()
+    {
+        isHeld = false;
+        Debug.Log("落としたよ");
+
+    }
+}
