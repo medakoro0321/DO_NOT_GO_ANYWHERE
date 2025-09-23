7d5de54b18e45c1e9c267381ca393efef20fc6a0
めだころ <153439300+medakoro0321@users.noreply.github.com>
2025-09-23 17:59:11 +0900

    markdownの変更とデスクトップ向けアニメーションのスクリプトを製作

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,13 +2,31 @@
 using Mono.Cecil.Cil;
 using TMPro;
 using UdonSharp;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 using VRC.SDKBase;
 using VRC.Udon;
 
 public class Test : UdonSharpBehaviour
 {
     private bool isHeld = false;
+    private VRC_Pickup pickup;
+
+    void Start()
+    {
+        pickup = (VRC_Pickup)GetComponent(typeof(VRC_Pickup));
+    }
+
+    void Update()
+    {
+        //desktop用
+        //左クリック判定
+        if (Input.GetMouseButtonDown(0) && isHeld)
+        {
+            Debug.Log("Clicked!");
+        }
+    }
 
     ///<summary>
     ///持った時呼ばれる
@@ -16,7 +34,7 @@ public class Test : UdonSharpBehaviour
     public override void OnPickup()
     {
         isHeld = true;
-        Debug.Log("持ったよ");
+        Debug.Log(pickup.currentPlayer);
     }
 
     /// <summary>
