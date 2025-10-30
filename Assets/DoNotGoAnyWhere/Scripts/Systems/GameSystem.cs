3a5d3dccea2bdd885fd30c81a4921cf1cecf4a51
めだころ <153439300+medakoro0321@users.noreply.github.com>
2025-10-30 22:07:27 +0900

    [feat]ゲーム開始のテレポートを追加

--- Assets/DoNotGoAnyWhere/Scripts/Systems/GameSystem.cs
+++ Assets/DoNotGoAnyWhere/Scripts/Systems/GameSystem.cs
@@ -1,12 +1,19 @@
 ﻿using UdonSharp;
 using UnityEngine;
+using VRC.SDKBase;
+using DoNotGoAnyWhere.Systems;
 
 namespace DoNotGoAnyWhere.Systems
 {
     public class GameSystem : UdonSharpBehaviour
     {
+        // 同期変数
+        [UdonSynced] public int timeRemaining = 0;
+        // 変数
+        private const int DefaultLimitSec = 60 * 5; // 5分 : 60(sec) * 5(min)
         public bool isReady = false;
         public bool isAutoStart = true;
+        private Transform _startSpawnPoint; // スタート時スポーンする場所
 
         // 外部スクリプトを読み込み
         private KillerSelect _killerSelect;
@@ -15,9 +22,10 @@ namespace DoNotGoAnyWhere.Systems
         private void Start()
         {
             _killerSelect = GetComponentInChildren<KillerSelect>();
-
             // TextTyperコンポーネントを取得
             _textTyper = GetComponent<TextTyper>();
+            // 残り時間を初期時間に設定
+            timeRemaining = DefaultLimitSec;
         }
 
         /// <summary>
@@ -25,12 +33,8 @@ namespace DoNotGoAnyWhere.Systems
         /// </summary>
         public void ReadyGame()
         {
-            // インスタンスメソッドとして呼び出す
-            _textTyper.Play("YOU RUN AWAY NOW!!!!!", 2f);
-
             _killerSelect.SelectKillers();
             isReady = true;
-
             // ゲーム自動開始
             if (isAutoStart) StartGame();
             else Debug.Log("Waiting for game to start");
@@ -41,7 +45,18 @@ namespace DoNotGoAnyWhere.Systems
         /// </summary>
         private void StartGame()
         {
+            var allPlayer = new VRCPlayerApi[VRCPlayerApi.GetPlayerCount()];
+            VRCPlayerApi.GetPlayers(allPlayer);
+            
             _textTyper.Play("YOU RUN AWAY NOW!!!!!", 2f);
+            foreach (var player in allPlayer)
+            {
+                if (!_killerSelect.IsKiller(player)) return;
+                player.TeleportTo(
+                    _startSpawnPoint.position,
+                    player.GetRotation()
+                    );
+            }
         }
     }
 }
